902760e2e8c5ffdba860daf40b365ee671b00af2
Chasialis Philip <ChasialisP@unisystems.gr>
2019-11-13 19:02:27 +0200

    Adding Snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,21 +2,29 @@ import React from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export default function Navbar(props) {
     const { level, changeLevel } = props;
     const [format, setFormat] = React.useState("hex");
+    const [open, setOpen] = React.useState(false);
 
     const handleChange = e => {
         setFormat(e.target.value);
+        setOpen(true);
         props.handleChange(e.target.value);
     };
 
     return (
         <header className="Navbar">
             <div className="logo">
+                {/* eslint-disable-next-line */}
                 <a href="#">React Color Picker</a>
             </div>
 
@@ -34,6 +42,21 @@ export default function Navbar(props) {
                     <MenuItem value="rgba">RGBA - rgb (255,255,255,1.0)</MenuItem>
                 </Select>
             </div>
+            <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={open}
+                autoHideDuration={3000}
+                message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
+                ContentProps={{
+                    "aria-describedby": "message-id"
+                }}
+                action={[
+                    <IconButton onClick={() => setOpen(false)} color="inherit" key="close" aria-label="close">
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+                onClose={() => setOpen(false)}
+            ></Snackbar>
         </header>
     );
 }
