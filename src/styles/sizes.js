52b5fd2f4a0bf41b58c8d42e08deb8ff6344037f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-24 19:44:29 +0200

    add responsive helper and style colorbox

---
+++ src/styles/sizes.js
@@ -0,0 +1,24 @@
+/*
+@media (max-widtrh: 575.98px)
+
+@media (max-widtrh: 767.98px)
+
+@media (max-widtrh: 991.98px)
+
+@media (max-widtrh: 1199.98px)
+
+
+*/
+
+export default {
+    up() {},
+    down(size) {
+        const sizes = {
+            xs: "575.98px",
+            sm: "767.98px",
+            md: "991.98px",
+            lg: "1199.98px"
+        };
+        return `@media (max-width: ${sizes[size]})`;
+    }
+};
